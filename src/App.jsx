952f1f4d817a976999948bc8f6ba9bdf01ea5684
R-Tedy <stedyroland2@gmail.com>
2025-06-28 03:28:08 +0300

    implement the about section

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <Cocktails/>
-      {/* <About/> */}
+      <About/>
       {/* <Art/> */}
       {/* <Menu/> */}
       {/* <Contact/> */}
