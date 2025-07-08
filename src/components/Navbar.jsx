7e354b58749e525c951ec79e8166d8e53cd83924
R-Tedy <stedyroland2@gmail.com>
2025-07-08 05:25:48 +0300

    impliment whatsapp chat interface

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {navLinks} from '../../constants/index'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
 
 const Navbar = () => {
   useGSAP(() => {
@@ -25,24 +26,35 @@ const Navbar = () => {
   })
 
   return (
-    <nav>
-      <div>
-        <a href="#home" className='flex items-center gap-2'>
-          <img src="/images/logo.png" alt="logo" />
-          <p>Madrinks poa</p>
-        </a>
+    <>
+      <nav>
+        <div>
+          <a href="#home" className='flex items-center gap-2'>
+            <img src="/images/logo.png" alt="logo" />
+            <p>Madrinks poa</p>
+          </a>
 
-        <ul>
-          {navLinks.map((link) => (
-            <li key={link.id}>
-              <a href={`#${link.id}`}>
-                {link.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
+          <ul>
+            {navLinks.map((link) => (
+              <li key={link.id}>
+                <a href={`#${link.id}`}>
+                  {link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+      <FloatingWhatsApp
+        phoneNumber='+254759693618'
+        accountName='Madrinks Poa'
+        avatar='/images/logo.png'
+        chatMessage='Hello, how can we be of help?'
+        placeholder='Type something...'
+        darkMode = {true}
+        notification='true'
+      />
+    </>
   )
 }
 
